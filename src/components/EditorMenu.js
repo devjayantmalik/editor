9db3ba8d374af487f708025855c91b84c12c8970
Jayant Malik <dev.jayantmalik@gmail.com>
2020-09-22 22:08:06 +0530

    added codemirror editor

--- src/components/EditorMenu.js
+++ src/components/EditorMenu.js
@@ -32,72 +32,132 @@ const EditorMenu = () => {
             <div className="navbar-link is-arrowless">File</div>
 
             <div className="navbar-dropdown">
-              <a className="navbar-item">New File</a>
-              <a className="navbar-item">Open</a>
-              <a className="navbar-item">Save</a>
+              <a href="javascript;" rel="noopener" className="navbar-item">
+                New File
+              </a>
+              <a href="javascript:;" className="navbar-item">
+                Open
+              </a>
+              <a href="javascript:;" className="navbar-item">
+                Save
+              </a>
               <hr className="navbar-divider" />
-              <a className="navbar-item">Export Project</a>
-              <a className="navbar-item">Import Project</a>
+              <a href="javascript:;" className="navbar-item">
+                Export Project
+              </a>
+              <a href="javascript:;" className="navbar-item">
+                Import Project
+              </a>
               <hr className="navbar-divider" />
-              <a className="navbar-item">Exit</a>
+              <a href="javascript:;" className="navbar-item">
+                Exit
+              </a>
             </div>
           </li>
 
           <li className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link is-arrowless">Edit</a>
+            <a href="javascript:;" className="navbar-link is-arrowless">
+              Edit
+            </a>
 
             <div className="navbar-dropdown">
-              <a className="navbar-item">Font</a>
-              <a className="navbar-item">Line Numbers</a>
+              <a href="javascript:;" className="navbar-item">
+                Font
+              </a>
+              <a href="javascript:;" className="navbar-item">
+                Line Numbers
+              </a>
             </div>
           </li>
 
           <li className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link is-arrowless">View</a>
+            <a href="javascript:;" className="navbar-link is-arrowless">
+              View
+            </a>
 
             <div className="navbar-dropdown">
-              <a className="navbar-item">Full Screen</a>
+              <a href="javascript:;" className="navbar-item">
+                Full Screen
+              </a>
               <hr className="dropdown-divider" />
-              <a className="navbar-item">Files Pane</a>
-              <a className="navbar-item">Output Pane</a>
+              <a href="javascript:;" className="navbar-item">
+                Files Pane
+              </a>
+              <a href="javascript:;" className="navbar-item">
+                Output Pane
+              </a>
             </div>
           </li>
 
           <li className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link is-arrowless">Project</a>
+            <a href="javascript:;" className="navbar-link is-arrowless">
+              Project
+            </a>
 
             <div className="navbar-dropdown">
-              <a className="navbar-item">All Projects</a>
-              <a className="navbar-item">Dependencies</a>
+              <a href="javascript:;" className="navbar-item">
+                All Projects
+              </a>
+              <a href="javascript:;" className="navbar-item">
+                Dependencies
+              </a>
               <hr className="dropdown-divider" />
-              <a className="navbar-item">Programming Language</a>
-              <a className="navbar-item">Archive</a>
+              <a href="javascript:;" className="navbar-item">
+                Programming Language
+              </a>
+              <a href="javascript:;" className="navbar-item">
+                Archive
+              </a>
               <hr className="dropdown-divider" />
-              <a className="navbar-item has-text-danger">Delete Project</a>
+              <a href="javascript:;" className="navbar-item has-text-danger">
+                Delete Project
+              </a>
             </div>
           </li>
 
           <li className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link is-arrowless">Account</a>
+            <a href="javascript:;" className="navbar-link is-arrowless">
+              Account
+            </a>
 
             <div className="navbar-dropdown">
-              <a className="navbar-item">Connect Github</a>
-              <a className="navbar-item">Connect Google</a>
+              <a href="javascript:;" className="navbar-item">
+                Connect Github
+              </a>
+              <a href="javascript:;" className="navbar-item">
+                Connect Google
+              </a>
               <hr className="dropdown-divider" />
-              <a className="navbar-item">Login via Email</a>
-              <a className="navbar-item">Register new Account</a>
+              <a href="javascript:;" className="navbar-item">
+                Login via Email
+              </a>
+              <a href="javascript:;" className="navbar-item">
+                Register new Account
+              </a>
               <hr className="dropdown-divider" />
-              <a className="navbar-item">Manage Profile</a>
-              <a className="navbar-item">Reset Password</a>
+              <a href="javascript:;" className="navbar-item">
+                Manage Profile
+              </a>
+              <a href="javascript:;" className="navbar-item">
+                Reset Password
+              </a>
             </div>
           </li>
           <li className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link is-arrowless">Help</a>
+            <a href="javascript:;" className="navbar-link is-arrowless">
+              Help
+            </a>
 
             <div className="navbar-dropdown">
-              <a className="navbar-item">About Editor</a>
-              <a className="navbar-item">Our Organisation</a>
-              <a className="navbar-item">Donate Us</a>
+              <a href="javascript:;" className="navbar-item">
+                About Editor
+              </a>
+              <a href="javascript:;" className="navbar-item">
+                Our Organisation
+              </a>
+              <a href="javascript:;" className="navbar-item">
+                Donate Us
+              </a>
             </div>
           </li>
         </ul>
--- src/components/EditorSection.js
+++ src/components/EditorSection.js
@@ -1,10 +1,59 @@
 import React from "react";
 import EditorMenu from "./EditorMenu";
+import Codemirror from "react-codemirror";
+
+// Required for code mirror styling
+import "codemirror/lib/codemirror.css";
+
+// Required for syntax hightlighting
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/markdown/markdown";
+
+// Codemirror values: Will update them later
+let markdownCode = `# Heading 1
+This is the information about the site. We are here alwasys to help you out. Thanks for the sample markdown file.
+
+## Heading 2
+
+\`\`\`java
+public class Human{
+	public static void main(String[] args){
+		System.out.println(\"Hello, World\");
+	}
+}
+\`\`\``;
+
+let code = `console.log("Hello, World!");
+
+// This is really great language.`;
 
 const EditorSection = () => (
   <section className="column editor-section h-100 p-0">
     <EditorMenu />
-    <div id="editor" className="overflow-scroll h-100 p-4"></div>
+    <div
+      id="editor"
+      style={{ fontSize: "22pt" }}
+      className="overflow-scroll h-100"
+    >
+      <Codemirror
+        value={markdownCode}
+        onChange={(value) => {
+          code = value;
+        }}
+        options={{
+          lineNumbers: true,
+          mode: "markdown",
+          lineWrapping: true,
+          smartIndent: true,
+          autofocus: true,
+          indentUnit: 4, // number of spaces per tab
+          indentWithTabs: true,
+          readOnly: false,
+          lint: true,
+        }}
+      />
+    </div>
   </section>
 );
 
