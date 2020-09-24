87248db15e96f718e2770b42f3162123bd4c9288
Jayant Malik <dev.jayantmalik@gmail.com>
2020-09-24 06:25:54 +0530

    Updates

---
+++ precache-manifest.042ab8d785d6dc07da7efd68362a3a11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c10af67c20cc0e93e7bfc0ed9e36326",
+    "url": "./index.html"
+  },
+  {
+    "revision": "827f0aca00aa3f919597",
+    "url": "./static/css/2.e5dd9e41.chunk.css"
+  },
+  {
+    "revision": "6366bbd852ad724a1d7d",
+    "url": "./static/css/main.398a74c8.chunk.css"
+  },
+  {
+    "revision": "827f0aca00aa3f919597",
+    "url": "./static/js/2.51293f70.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.51293f70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6366bbd852ad724a1d7d",
+    "url": "./static/js/main.485760eb.chunk.js"
+  },
+  {
+    "revision": "9581eb836ab0cd38ab2d",
+    "url": "./static/js/runtime-main.8cf48bc5.js"
+  },
+  {
+    "revision": "e95b2e68e3e42ff1fabaf8ca633258de",
+    "url": "./static/media/logo-light.e95b2e68.svg"
+  }
+]);
